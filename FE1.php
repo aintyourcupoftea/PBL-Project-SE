286a39bca6f41c815268c242e39970e4322473b2
aintyourcupofteae <amitbabangavali@gmail.com>
2022-05-10 13:51:33 +0530

    comit 4
    Signed-off-by: aintyourcupofteae <amitbabangavali@gmail.com>

---
+++ FE1.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Animated-Type-Heading.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/Perfect-navigation-bar.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+    <link rel="stylesheet" href="assets/css/Section-Title.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/fe1.css">
+    
+
+    <title>Document</title>
+
+<?php
+
+
+
+include "db_student1.php";
+
+$sql = "SELECT * FROM `std` WHERE `year` LIKE 'FE'";
+$result = mysqli_query($conn, $sql);
+
+// Find the number of records returned
+$num = mysqli_num_rows($result);
+?>
+
+<body>
+<div class="caption v-middle text-center">
+					<h1 class="cd-headline clip">
+                    <a class="heading1" href="home.php">
+			            <span class="blc">Student | </span>
+			            <span class="cd-words-wrapper">
+			              <b class="is-visible">DBMS.</b>
+			            </span>
+                    </a>
+	          		</h1>
+				</div>
+    <nav class="navbar navbar-light navbar-expand-lg navigation-clean-search">
+        <div class="container"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-2">
+                <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="registrationForm.php">Register New Student</a></li>
+                    <li class="nav-item"><a class="nav-link" href="updateDetails.html">Update Details</a></li>
+                </ul>
+                <form class="me-auto search-form" target="_self">
+                    <div class="d-flex align-items-center"><label class="form-label d-flex mb-0" for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" id="search-field" name="search"></div>
+                </form><a class="btn btn-light action-button" role="button" href="logout.php">Log Out </a>
+            </div>
+        </div>
+    </nav>
+    
+<div class="container">
+  <h2>FE STUDENTS <small><?php
+echo "      (Number of Students in FE :";
+echo $num." )";
+
+  ?></small></h2>
+  <ul class="responsive-table">
+    <li class="table-header">
+      <div class="col col-1">Roll Number</div>
+      <div class="col col-2">Name</div>
+      <div class="col col-3">Email</div>
+      <div class="col col-4">Year</div>
+    </li>
+
+</body>
+
+
+
+<?php
+echo "<br>";
+
+if($num> 0){
+    while($row = mysqli_fetch_assoc($result)){
+?> 
+
+<!-- <div class="container"> -->
+  
+  <ul class="responsive-table">
+   
+    <li class="table-row">
+
+      <div class="col col-1" data-label="Job Id">  <?php echo $row['rollno'];?></div>
+       
+        
+      <div class="col col-2" data-label="Customer Name"><?php  echo $row['name']; ?></div>
+
+
+        
+      <div class="col col-3" data-label="Amount"><?php echo $row['email']; ?></div> 
+
+      <div class="col col-4" data-label="Payment Status"><?php  echo $row['year']; ?></div>
+      
+
+      </li>
+      </ul>
+<!-- </div> -->
+    
+  
+<?php    
+}
+
+
+}
+?>
+
+
+</head>
+<body>
+    
+</body>
+</html>
---
+++ updateDetails.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+	 	<html lang="en">
+	 	
+	 	<head>
+	 	    <meta charset="utf-8">
+	 	    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	 	    <title>Register New Student</title>
+	 	    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	 	    <link rel="stylesheet" href="assets/css/RegistrationForm.css">
+	 	    <link rel="stylesheet" href="assets/css/styles.css">
+        <script src="myscripts.js"></script>
+	 	</head>
+
+
+		 <?php
+
+      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+          
+
+			$roll_no = $_POST['rollno'];
+			$marks = $_POST['marks'];
+			
+			
+
+	include "db_student1.php";
+
+      $sql = "SELECT * FROM `std` WHERE `rollno` LIKE '$roll_no'";
+      $result = mysqli_query($conn,$sql);
+      $num = mysqli_num_rows($result);
+      if($num> 0){
+       if($row = mysqli_fetch_assoc($result)){
+
+       if($marks> 0){
+
+        if($row['year']=='FE'){
+          $sql1 = "UPDATE `std` SET `year` = 'SE' WHERE `std`.`rollno` = $roll_no";
+          $result1 = mysqli_query($conn,$sql1);
+		  $sql_1 = "UPDATE `std` SET `cgpa1` = '$marks' WHERE `std`.`rollno` = $roll_no";
+		  $result_1= mysqli_query($conn,$sql_1);
+
+          ?> <div  class="alert alert-success alert-dismissible fade show" role="alert">
+           <strong>Done!</strong> <?php echo $row['name']; echo " promoted to the Second year"; 
+             ?><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+           </div>;
+           <?php
+        }
+
+        if($row['year']=='SE'){
+         $sql2 =   "UPDATE `std` SET `year` = 'TE' WHERE `std`.`rollno` = $roll_no";
+         $result = mysqli_query($conn, $sql2);
+		 $sql_2 = "UPDATE `std` SET `cgpa2` = '$marks' WHERE `std`.`rollno` = $roll_no";
+		 $result_2= mysqli_query($conn,$sql_2);
+
+         ?> <div  class="alert alert-success alert-dismissible fade show" role="alert">
+         <strong>Done!</strong> <?php echo $row['name']; echo " promoted to the Third year"; 
+           ?><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+         </div>;
+         <?php
+
+        }
+
+        if($row['year']=='TE'){
+          $sql2 =   "UPDATE `std` SET `year` = 'BE' WHERE `std`.`rollno` = $roll_no";
+          $result = mysqli_query($conn, $sql2);
+		  $sql_3 = "UPDATE `std` SET `cgpa3` = '$marks' WHERE `std`.`rollno` = $roll_no";
+		  $result_3= mysqli_query($conn,$sql_3);
+ 
+          ?> <div  class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Done!</strong> <?php echo $row['name']; echo " promoted to the Fourth year"; 
+            ?><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>;
+          <?php
+ 
+         }
+
+		 if($row['year']=='BE'){
+			$sql2 =   "UPDATE `std` SET `year` = 'PO' WHERE `std`.`rollno` = $roll_no";
+			$result = mysqli_query($conn, $sql2);
+			$sql_4 = "UPDATE `std` SET `cgpa4` = '$marks' WHERE `std`.`rollno` = $roll_no";
+			$result_4= mysqli_query($conn,$sql_4);
+   
+			?> <div  class="alert alert-success alert-dismissible fade show" role="alert">
+			<strong>Done!</strong> <?php echo $row['name']; echo " Completed BE "; 
+			  ?><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>;
+			<?php
+   
+		   }
+   
+
+      }
+      else{ 
+        ?>
+        <div  class="alert alert-danger d-flex align-items-center" role="alert">
+				  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+				 <div>
+				 	<?php echo $row['name']; echo " should clear Exam to promote in next year"; ?>
+			   </div>
+				 </div>'
+     <?php
+      }
+       
+			  
+
+			  // if($result){
+				//   echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+				//   <strong>Done!</strong> Data Submitted Successfully.
+				//   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+				// </div>';
+			  // }
+
+
+        
+			  // else{
+				//   echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
+				//   <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+				//   <div>
+				// 	Data Not Submitted
+				//   </div>
+				// </div>';
+			
+        // }\
+        }  
+      }
+			}
+		  
+
+      ?>
+
+
+	 	
+	 	<body>
+	 	    <div class="row register-form">
+	 	        <div class="col-md-8 offset-md-2">
+	 	            <form  action = "updateDetails.php"  method = "post" class="custom-form">
+	 	                <h1>Update Profile of Student</h1>
+	 	                <div class="row form-group">
+	 	                    <div class="col-sm-4 col-md-4 label-column"></div>
+	 	                    <div class="col-sm-6 input-column"></div>
+	 	                </div>
+	 	                <div class="row form-group">
+	 	                    <div class="col-sm-4 label-column"><label class="col-form-label" for="rollno">Roll Number</label></div>
+	 	                    <div class="col-sm-6 input-column"><input name = "rollno" id ="rollno" class="form-control" type="number" required=""></div>
+	 	                </div>
+	 	                <div class="row form-group">
+	 	                    <div class="col-sm-4 label-column"><label class="col-form-label" for="marks">Sgpa</label></div>
+	 	                    <div class="col-sm-6 input-column"><input name="marks" id = "marks" class="form-control" type="number"step="any" required="" pattern="[a-z0-9.%+_]+@moderncoe.edu.in"  title="Please enter the email with domain moderncoe.edu.in">
+						</div>
+					
+	 	                </div>
+	 	                <div class="row form-group">
+	 	                    <div class="col-sm-4 label-column">
+	 	                        
+	 	
+	 	                    
+	 	                </div>
+	 	                    
+	 	                    
+	 	                <!-- </div><a class="btn btn-light submit-button" role="button">Submit</a> -->
+						 </div>
+						 <button type="submit" class="btn btn-light submit-button" >Submit</button>
+						 
+	 	            </form>
+	 	        </div>
+	 	    </div>
+	 	    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	 	</body>
+	 	
+	 	</html>
+
